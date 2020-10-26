104b3b1b79e65e8bdea8273f28a33bbedfb55014
DavideGit991 <pasto.davide1991@gmail.com>
2020-10-26 14:14:06 +0100

    esercizio in debug completo

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// FizzBuzz
+// Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+
+
+// creazione array di 100 numeri
+var numeri = [];
+var numero = 0;
+
+for (var i = 1; i <101; i++)
+{
+  numeri.push(numero +(numero+i));
+}
+console.log(numeri);
+
+
+for (var i = 0; i <100; i++)
+{
+
+  //sostituisco "FizzBUZZ" ai multipli di 3 e 5
+   if(numeri[i] % 3 === 0 && numeri[i] % 5 === 0)
+   {
+     numeri[i]= "FizzBuzz";
+   }
+    //sostituisco "Fizz" ai multipli di 3
+   else if(numeri[i] % 3 === 0)
+   {
+     numeri[i]= "Fizz";
+   }
+
+   //sostituisco "Buzz" ai multipli di 5
+    else if(numeri[i] % 5 === 0)
+    {
+     numeri[i]= "Buzz";
+    }
+
+    console.log(numeri[i]);
+}
